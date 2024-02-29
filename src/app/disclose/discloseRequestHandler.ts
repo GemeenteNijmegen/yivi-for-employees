296d05bf78d2bb278b868c0cd6f0acbb3040f236
Marnix Dessing <m.dessing@nijmegen.nl>
2024-02-29 19:21:59 +0100

    feat: add multiple use cases

--- src/app/disclose/discloseRequestHandler.ts
+++ src/app/disclose/discloseRequestHandler.ts
@@ -3,10 +3,19 @@ import { Response } from '@gemeentenijmegen/apigateway-http/lib/V2/Response';
 import { Session } from '@gemeentenijmegen/session';
 import { render } from '@gemeentenijmegen/webapp';
 import * as dislosureTemplate from './templates/disclose.mustache';
+import * as dislosureTemplateAge from './templates/discloseResultAge.mustache';
+import * as dislosureTemplateEmployee from './templates/discloseResultEmployee.mustache';
+import * as dislosureTemplateKoffie from './templates/discloseResultKoffie.mustache';
 import { YiviApi } from '../util/YiviApi';
 
 const yivi = new YiviApi();
 const init = yivi.init();
+
+export interface DiscloseRequestHandlerRequest {
+  cookies: string;
+  result: boolean;
+  type?: string;
+}
 
 export class DiscloseRequestHandler {
   private dynamoDBClient: DynamoDBClient;
@@ -14,39 +23,32 @@ export class DiscloseRequestHandler {
     this.dynamoDBClient = dynamoDBClient;
   }
 
-  async handleRequest(cookies: string, getResult: boolean) {
+  async handleRequest(params: DiscloseRequestHandlerRequest) {
     // Make sure we are initalized
     await init;
 
     // Check if we have a session
-    let session = new Session(cookies, this.dynamoDBClient, {
+    let session = new Session(params.cookies, this.dynamoDBClient, {
       ttlInMinutes: parseInt(process.env.SESSION_TTL_MIN ?? '15'),
     });
     const hasSession = await session.init();
 
-    if (getResult && hasSession !== false) {
-      return this.handleDisclosureResultRequest(session);
+    if (params.result && hasSession !== false) {
+      return this.handleDisclosureResultRequest(session, params);
     }
 
-    return this.handleStartDisclosureRequest(session);
+    return this.handleStartDisclosureRequest(session, params);
   }
 
-  private async handleStartDisclosureRequest(session: Session) {
+  private async handleStartDisclosureRequest(session: Session, params: DiscloseRequestHandlerRequest) {
 
     // 1. start yivi session
     let base64YiviSession = undefined;
     let requestorToken = undefined;
     let error = undefined;
     try {
-      const disclosureSession = await yivi.startDisclosureSession(
-        [ // And
-          [ // Or
-            [ // List of attributes
-              'irma-demo.gemeente.personalData.bsn',
-            ],
-          ],
-        ],
-      );
+      const request = disclosureRequestForType(params.type);
+      const disclosureSession = await yivi.startDisclosureSession(request);
       if (disclosureSession.error) {
         throw new Error(`Error response from yivi api client: ${disclosureSession.error}`);
       }
@@ -84,7 +86,7 @@ export class DiscloseRequestHandler {
 
   }
 
-  private async handleDisclosureResultRequest(session: Session) {
+  private async handleDisclosureResultRequest(session: Session, params: DiscloseRequestHandlerRequest) {
 
     // 1. Get requestorToken from user session
     const requestorToken = session.getValue('token');
@@ -99,11 +101,7 @@ export class DiscloseRequestHandler {
       const response = await yivi.getSessionResult(requestorToken);
       validateDisclosureResponse(response);
 
-      const disclosedAttributeBsn = response.disclosed[0][0];
-      result = {
-        attr: disclosedAttributeBsn.id,
-        value: disclosedAttributeBsn.rawvalue,
-      };
+      result = handleDisclosureForType(response, params.type);
 
     } catch (err) {
       console.error(err);
@@ -114,13 +112,13 @@ export class DiscloseRequestHandler {
     const data = {
       title: 'Check',
       shownav: false,
-      yiviServer: `https://${yivi.getHost()}`,
       result: result,
       error: error,
     };
 
     // render page
-    const html = await render(data, dislosureTemplate.default);
+    const template = templateForRequestType(params.type);
+    const html = await render(data, template);
     return Response.html(html, 200, session.getCookie());
   }
 }
@@ -135,5 +133,63 @@ function validateDisclosureResponse(result: any) {
   }
   if (!result.disclosed) {
     throw new Error('No data is disclosed in the session');
+  }
+}
+
+function handleDisclosureForType(result: any, type?: string) {
+  switch (type) {
+    case 'employee':
+      return result.disclosed[0][0].rawValue;
+    case 'koffie':
+      return result.disclosed[0][0].rawValue;
+    case 'age':
+    case undefined:
+    default:
+      return result.disclosed[0][0].rawValue;
+  }
+}
+
+
+function templateForRequestType(type?: string) {
+  switch (type) {
+    case 'employee':
+      return dislosureTemplateEmployee.default;
+    case 'koffie':
+      return dislosureTemplateKoffie.default;
+    case 'age':
+    case undefined:
+    default:
+      return dislosureTemplateAge.default;
+  }
+}
+
+function disclosureRequestForType(type?: string) {
+  switch (type) {
+    case 'employee':
+      return [ // And
+        [ // Or
+          [ // List of attributes
+            'irma-demo.nijmegen.employeeData.works-for-gemeente-nijmegen',
+          ],
+        ],
+      ];
+    case 'koffie':
+      return [ // And
+        [ // Or
+          [ // List of attributes
+            'irma-demo.nijmegen.employeeData.email',
+          ],
+        ],
+      ];
+    case 'age':
+    case undefined:
+    default:
+      return [ // And
+        [ // Or
+          [ // List of attributes
+            'irma-demo.gemeente.personalData.over18',
+          ],
+        ],
+      ];
   }
 }
