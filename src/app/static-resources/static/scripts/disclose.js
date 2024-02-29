7bdaa83e451567741b4b15450e493037d80732bd
Marnix Dessing <m.dessing@nijmegen.nl>
2024-02-29 16:26:40 +0100

    feat: disclose using backend

--- src/app/static-resources/static/scripts/disclose.js
+++ src/app/static-resources/static/scripts/disclose.js
@@ -1,36 +1,43 @@
 const yiviServer = document.getElementById("yiviServer").getAttribute('data');
+// Get the session response
+const yiviFullSessionEncoded = document.getElementById("yiviFullSession").getAttribute('data');
 
-const yiviClient = yivi.newWeb({
-    debugging: false,
-    element: '#yivi-web-form',
-    session: {
-        url: yiviServer,
-        //start: false, // No need to start session from the browser (done server-sied)
-        // mapping: {
-        //     sessionPtr: r => yiviFullSession.sessionPtr,
-        //     sessionToken: r => yiviFullSession.token,
-        //     frontendRequest: r => yiviFullSession.frontendRequest
-        // },
-        result: true, // No need to fetch session result (status success / failed is sufficient)
-    },
-    state: {
-        serverSentEvents: false,
-        frontendOptions: {
-            endpoint: 'options',
-            requestContext: 'https://irma.app/ld/request/frontendoptions/v1'
+if (yiviFullSessionEncoded) {
+
+    const yiviFullSession = JSON.parse(atob(yiviFullSessionEncoded));
+
+    const yiviClient = yivi.newWeb({
+        debugging: false,
+        element: '#yivi-web-form',
+        session: {
+            url: yiviServer,
+            start: false, // No need to start session from the browser (done server-sied)
+            mapping: {
+                sessionPtr: r => yiviFullSession.sessionPtr,
+                sessionToken: r => yiviFullSession.token,
+                frontendRequest: r => yiviFullSession.frontendRequest
+            },
+            result: false, // No need to fetch session result (status success / failed is sufficient)
         },
-        pairing: {
-            onlyEnableIf: m => yiviFullSession.frontendRequest.pairingHint,
-            completedEndpoint: 'pairingcompleted',
-            minCheckingDelay: 500, // Minimum delay before accepting or rejecting a pairing code, for better user experience.
-            pairingMethod: 'pin'
+        state: {
+            serverSentEvents: false,
+            frontendOptions: {
+                endpoint: 'options',
+                requestContext: 'https://irma.app/ld/request/frontendoptions/v1'
+            },
+            pairing: {
+                onlyEnableIf: m => yiviFullSession.frontendRequest.pairingHint,
+                completedEndpoint: 'pairingcompleted',
+                minCheckingDelay: 500, // Minimum delay before accepting or rejecting a pairing code, for better user experience.
+                pairingMethod: 'pin'
+            }
         }
-    }
-});
-
-yiviClient.start()
-    .then(() => console.log('Issuing completed!'))
-    .catch((err) => {
-        document.getElementById('retry-button').classList.remove('hidden');
-        console.error("Could not complete issuing session", err);
     });
+
+    yiviClient.start()
+        .then(() => console.log('Disclosing completed!'))
+        .catch((err) => {
+            document.getElementById('retry-button').classList.remove('hidden');
+            console.error("Could not complete issuing session", err);
+        });
+}
